10961de1f1899bcd89872ac84c3337b32433ea88
Guhyun <162582453+kgh852@users.noreply.github.com>
2024-07-18 08:36:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
